ed4e876265d34b8ce56eeb49a37791f7390709ff
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-09-25 11:38:50 +0200

    add storing the user in database

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import {ConflictException, Injectable} from '@nestjs/common';
 import {PrismaService} from "../../prisma/prisma.service";
 import * as bcrypt from "bcryptjs"
+import {UserType} from "@prisma/client";
 
 interface SignupParams {
     email: string;
@@ -14,19 +15,29 @@ export class AuthService {
     constructor(private readonly prismaService: PrismaService) {
     }
 
-    async signup({email, password}: SignupParams) {
+    async signup({email, password, name, phone}: SignupParams) {
         const userExists = await this.prismaService.user.findUnique({
             where: {
                 email
             }
         })
 
-        if(userExists){
-            throw new  ConflictException()
+        if (userExists) {
+            throw new ConflictException()
         }
 
-        const hashedPassword = await  bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10)
 
-        console.log({hashedPassword})
+        const user = await this.prismaService.user.create({
+            data: {
+                email,
+                name,
+                phone,
+                password: hashedPassword,
+                user_type: UserType.BUYER
+            }
+        });
+
+        return user
     }
 }
