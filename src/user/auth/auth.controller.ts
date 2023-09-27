907935cfe9b0162b7d979a95f0f77c438bbe3b98
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-09-27 22:46:47 +0200

    add singin function

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import {AuthService} from "./auth.service";
-import {SignupDto} from "../dtos/auth.dto";
+import { AuthService } from './auth.service';
+import { SignupDto } from '../dtos/auth.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('/signup')
+  signup(@Body() body: SignupDto) {
+    return this.authService.signup(body);
+  }
 
-    constructor(private  readonly authService: AuthService) {
-    }
-    @Post ('/signup')
-    signup(
-        @Body() body: SignupDto
-    ){
-        return this.authService.signup(body)
-    }
-
+  @Post('/signin')
+  signin(@Body() body: SignupDto) {
+    return this.authService.signup(body);
+  }
 }
