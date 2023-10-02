fe24e0cf4d62fac9f9b9e82e1f17733d3f01a678
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-10-02 11:52:04 +0200

    add service to show all homes

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class HomeService {}
+export class HomeService {
+  constructor(private readonly prismaService: PrismaService) {}
+  getHomes() {
+    return this.prismaService.home.findMany();
+  }
+}
