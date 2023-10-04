3694a6336992ef8b4dd897ad17b28a5d1ed6f490
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-10-04 13:37:00 +0200

    fetching all homes

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { HomeResponseDto } from './dto/home.dto';
 
 @Injectable()
 export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
-  getHomes() {
-    return this.prismaService.home.findMany();
+  async getHomes(): Promise<HomeResponseDto[]> {
+    const homes = await this.prismaService.home.findMany();
+    return homes.map((home) => new HomeResponseDto(home));
   }
 }
